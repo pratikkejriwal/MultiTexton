5e15da61c064149fbca82fa28a1d85e851662364
Pratik Kejriwal <pratikkejriwal6292@gmail.com>
2018-02-09 05:45:58 +0530

    final

--- retrieval.py
+++ retrieval.py
@@ -1,7 +1,9 @@
+
 # coding: utf-8
 
 # # Image Retrieval based on Multi-Texton Histogram
 
+# In[1]:
 
 
 #importing libraries
@@ -10,57 +12,73 @@ import matplotlib.pyplot as plt
 import cv2
 import operator
 from IPython.display import Image
-import math
+
+
+# In[2]:
 
 
 from pymongo import MongoClient
 client = MongoClient('mongodb://localhost:27017')
-collection = client.test_database.coral
+collection = client.MTH.coralTest
+
+
+# # taking input from the User
+
+# In[3]:
+
 
 db = []
+path = "D:/SEM 6/Content/Lab_Activity/Corel 1K/image.orig/"
 num = np.int64(input('Enter the input Image: '))
 for x in collection.find():
     db = np.array(x['distances'])
+Image(path + str(num)+'.jpg')
 
-#Image(str(num)+'.jpg')
 
-inputImage = db[num]
+# In[7]:
+
+
+inputImage = db[int(num)-1]
 
 distance = np.zeros(1000*82).reshape(1000,82)
-
-distance = abs(db - inputImage)/(1 + db + inputImage)
+for i in range(1000):
+    for j in range(82):
+        distance[i,j] = abs(db[i,j] - inputImage[j])/(1 + db[i,j] + inputImage[j])
 
 distanceSum = np.sum(distance,axis=1)
 
-# In[19]:
+
+# In[8]:
+
 
 keys = np.arange(len(distanceSum),dtype=int)
 
 Imagedictionary = dict(zip(keys, distanceSum))
 sorted_images = sorted(Imagedictionary.items(), key=operator.itemgetter(1))
 
-# In[22]:
+
+# In[9]:
+
 
 i = 0;
-#Resultimages = []
-#ResultHists = np.zeros(1*82).reshape(1,82)
+Resultimages = []
+ResultHists = np.zeros(1000*82).reshape(1000,82)
 for key in sorted_images:
     if(i<=20):
-        #ResultHists[i]=db[key[0]]
+        print(key)
+        ResultHists[i]=db[key[0]]
         i = i +1
-        imageName = str(key[0])+'.jpg'
-        #Resultimages.append(imageName)
+        imageName = path + str(key[0])+'.jpg'
+        Resultimages.append(imageName)
         print (imageName)
-        cv2.imshow(str(i), cv2.imread(imageName))
     else:
         break;
 
-cv2.waitKey(0)
 
-# In[23]:
+# In[10]:
 
 
-#for ima in Resultimages:
- #   imageD = Image(ima)
-  #  display(imageD)
+for ima in Resultimages:
+    imageD = Image(ima)
+    display(imageD)
 
